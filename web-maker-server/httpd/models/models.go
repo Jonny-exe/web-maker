20bee55824cca94a34d68c08ed9a84e4fca4542b
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2020-12-27 15:12:14 +0100

    Added a basic functioning content object -> html

--- web-maker-server/httpd/models/models.go
+++ web-maker-server/httpd/models/models.go
@@ -1,24 +1,31 @@
 package models
 
+// TokenAndRecovery_key ...
 type TokenAndRecovery_key struct {
 	Token        string `json:"token"`
 	Recovery_key string `json:"recovery_key"`
 }
 
+// TokenAndObject ..
 type TokenAndObject struct {
 	Token  string      `json:"token"`
 	Object interface{} `json:"object"`
 }
 
+// Recovery_key ...
 type Recovery_key struct {
 	Recovery_key string `json:"recovery_key"`
 }
 
+// Token ...
 type Token struct {
 	Token string `json:"token"`
 }
 
+// Content ...
 type Content []ContentItem
+
+// ContentItem ...
 type ContentItem struct {
 	Type     string      `json:"type"`
 	Text     string      `json:"text"`
