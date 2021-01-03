d8b87979b5001a5ac36e32c450833cb4e61e481b
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-01-03 23:07:35 +0100

    hook test

--- web-maker/src/Alerts.tsx
+++ web-maker/src/Alerts.tsx
@@ -1,22 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react"
 
 const Alerts = (props: any) => {
-  const [alertsActive, setAlertsActive] = useState(false);
+  const [alertsActive, setAlertsActive] = useState(false)
   const showAlert = () => {
-    setAlertsActive(true);
-    setTimeout(() => setAlertsActive(false), 7000);
-  };
+    setAlertsActive(true)
+    setTimeout(() => setAlertsActive(false), 7000)
+  }
+
   useEffect(() => {
     const interval = setInterval(() => {
-      showAlert();
-    }, 20 * 60000);
-    return () => clearInterval(interval);
-  }, []);
+      showAlert()
+    }, 20 * 60000)
+    return () => clearInterval(interval)
+  }, [])
   return (
-    <div className={`informationDiv ${alertsActive ? 'alert' : 'hideAlert'}`}>
+    <div className={`informationDiv ${alertsActive ? "alert" : "hideAlert"}`}>
       Remember that you have to save your file. It wont save itself.
     </div>
-  );
-};
+  )
+}
 
-export default Alerts;
+export default Alerts
