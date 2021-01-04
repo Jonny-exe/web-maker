0b147366d128985edbfd470216c2788b491f8a61
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-01-04 12:27:30 +0100

    Made better names in RenderContent in web-maker

--- web-maker/src/RenderContent.tsx
+++ web-maker/src/RenderContent.tsx
@@ -3,94 +3,100 @@ import { notEditable } from "./exceptionItems"
 import RenderContent1 from "./RenderContent1"
 
 export const RenderContent = (props: any) => {
-	const [holdCounter, setHoldCounter] = useState(-1)
-	const [cancelHover, setCancelHover] = useState(false)
-	useEffect(() => {
-		if (props.itemIndex !== -1 && holdCounter !== -1) {
-			if (!cancelHover) {
-				if (holdCounter < 4) {
-					var timer = setTimeout(increaseHoldIndex, 75)
-				} else {
-					setHoldCounter(-1)
-					props.setModalEditPlacementActive(true)
-					setCancelHover(true)
-				}
-			} else {
-				setHoldCounter(-1)
-			}
-		}
-		return () => {
-			clearTimeout(timer)
-		}
-	}, [holdCounter])
+  const [holdCounter, setHoldCounter] = useState(-1)
+  const [cancelHover, setCancelHover] = useState(false)
+  useEffect(() => {
+    if (props.itemIndex !== -1 && holdCounter !== -1) {
+      if (!cancelHover) {
+        if (holdCounter < 4) {
+          var timer = setTimeout(increaseHoldIndex, 75)
+        } else {
+          setHoldCounter(-1)
+          props.setModalEditPlacementActive(true)
+          setCancelHover(true)
+        }
+      } else {
+        setHoldCounter(-1)
+      }
+    }
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [holdCounter])
 
-	const increaseHoldIndex = () => {
-		setHoldCounter(holdCounter + 1)
-	}
+  const increaseHoldIndex = () => {
+    setHoldCounter(holdCounter + 1)
+  }
 
-	console.log(
-		props.content[0] != undefined ? props.content[0].type === "img" : "empty",
-	)
-	return (
-		<>
-			{props.content.map((x: any, i: number) =>
-				x.type === "img" ? (
-					<img src={x.src} style={x.style} alt="Image" />
-				) : (
-					React.createElement(
-						x.type,
-						{
-							style: x.style,
-							contentEditable:
-								notEditable.includes(x.type) || props.previewMode
-									? "false"
-									: "true", // this is has to be like this because if not it doesnt detect the td only the table
-							key: i,
-							placeholder:
-								(notEditable.includes(x.type) && x.type !== "div") ||
-								!props.previewMode
-									? x.text
-									: "",
-							content: "",
-							onInput: (e: any) => {
-								x.content = e.target.textContent
-								console.log("ONINPUT")
-							},
-							onClick: (e: any) => {
-								props.setSavedStyle(e.target.style)
-							},
-							onMouseDown: (e: any) => {
-								if (props.setItemIndex !== undefined) {
-									props.setItemIndex(i)
-								}
-								increaseHoldIndex()
-								setCancelHover(false)
-							},
-							onMouseUp: (e: any) => {
-								setHoldCounter(-1)
-								setCancelHover(true)
-							},
-						},
-						x.children !== null &&
-							x.children !== undefined &&
-							x.children.length !== 0 ? (
-							<RenderContent1
-								content={x.children}
-								setModalEditPlacementActive={props.setModalEditPlacementActive}
-								previewMode={props.previewMode}
-								setSavedStyle={props.setSavedStyle}
-								setItemIndex={props.setItemIndex}
-								itemIndex={props.itemIndex}
-								savedStyle={props.savedStyle}
-							/>
-						) : (
-							x.content
-						),
-					)
-				),
-			)}
-		</>
-	)
+  const handleMouseDown = (index: number) => {
+    if (props.setItemIndex !== undefined) {
+      props.setItemIndex(index)
+    }
+    increaseHoldIndex()
+    setCancelHover(false)
+  }
+
+  return (
+    <>
+      {props.content.map((item: any, index: number) =>
+        item.type === "img" ? (
+          <img
+            src={item.src}
+            style={item.style}
+            onMouseDown={() => handleMouseDown(index)}
+            alt="Image"
+          />
+        ) : (
+          React.createElement(
+            item.type,
+            {
+              style: item.style,
+              contentEditable:
+                notEditable.includes(item.type) || props.previewMode
+                  ? "false"
+                  : "true", // this is has to be like this because if not it doesnt detect the td only the table
+              key: index,
+              placeholder:
+                (notEditable.includes(item.type) && item.type !== "div") ||
+                !props.previewMode
+                  ? item.text
+                  : "",
+              content: "",
+              onInput: (e: any) => {
+                item.content = e.target.textContent
+                console.log("ONINPUT")
+              },
+              onClick: (e: any) => {
+                props.setSavedStyle(e.target.style)
+              },
+              onMouseDown: (e: any) => {
+                handleMouseDown(index)
+              },
+              onMouseUp: (e: any) => {
+                setHoldCounter(-1)
+                setCancelHover(true)
+              },
+            },
+            item.children !== null &&
+              item.children !== undefined &&
+              item.children.length !== 0 ? (
+              <RenderContent1
+                content={item.children}
+                setModalEditPlacementActive={props.setModalEditPlacementActive}
+                previewMode={props.previewMode}
+                setSavedStyle={props.setSavedStyle}
+                setItemIndex={props.setItemIndex}
+                itemIndex={props.itemIndex}
+                savedStyle={props.savedStyle}
+              />
+            ) : (
+              item.content
+            ),
+          )
+        ),
+      )}
+    </>
+  )
 }
 
 export default RenderContent
